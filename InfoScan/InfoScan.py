5c0873fca5a11c4756ad6e026232d88ef5992db7
CyzCc <56634784+chengyizhou147@users.noreply.github.com>
2020-10-13 15:55:25 +0800

    Update InfoScan.py

--- InfoScan/InfoScan.py
+++ InfoScan/InfoScan.py
@@ -12,8 +12,7 @@ from common import *
 import sys
 
 def main():
-    url = Url(sys.argv[1])      #处理域名
-    print(get_domain_root(url))
+    print('当前域名'+get_domain_root(url))
     # # ============cms查询============== # #
     result_cms = Cms(domain = get_domain_root(url)).run()
     # # ============网站备案信息============== # #
@@ -37,7 +36,7 @@ def main():
 
     print("[*]整合结果中(去重)[*]")
     print_try("[*]整合完成，共"+str(len(result_chaxun))+"个域名[*]")
-    time.sleep(2)
+    #time.sleep(2)
 
     print("查询结果：\n")
     print("======备案信息======")
@@ -46,10 +45,13 @@ def main():
     print(result_cms)
     print("\n======子域名======")
     print_result(result_chaxun)
+    return result_chaxun
+
+def brute_main():
 
     # ============子域名爆破============== #
-    user_input()
-    result_baopo = Brutedomain(domain = get_domain_root(url)).run()
+
+    result_baopo = Brutedomain(domain = get_domain_root(url),thread_count=args.thread).run()
     # 将所有列表合并，方便去重整理结果
     result_end(result_baopo)    #爆破结果
     result_all = list(set(resultall))    #去重后的结果
@@ -70,4 +72,23 @@ if __name__ == '__main__':
     print("Use: python3 InfoScan.py domain Thread")
     print("Example: python3 InfoScan.py baidu.com 100")
     print("Example: python3 InfoScan.py baidu.com 100 > result.txt")
-    main()
+    args = parse_args()
+    if args.file == None:
+        url = Url(args.url)
+        main()
+        user_input(url,list(set(resultall)))
+        brute_main()
+        exit()
+    else:
+        for i in open(args.file,'r'):
+            url = i.replace('\n','')
+            main()
+            Baocun(url,list(set(resultall)))
+            resultall.clear()       # 清空列表，方便保存下一个url域名
+        user_input2()
+        for j in open(args.file,'r'):
+            url = j.replace('\n', '')
+            brute_main()
+            resultall.clear()
+    End()
+    exit()
